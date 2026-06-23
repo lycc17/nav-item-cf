86aab9b80f227fba32e387a5212dd3bfcc4273ce
lycc17 <155903423+lycc17@users.noreply.github.com>
2026-06-23 16:38:49 +0800

    Update Admin.vue

--- web/src/views/Admin.vue
+++ web/src/views/Admin.vue
@@ -84,7 +84,7 @@
         <UserManage v-if="page==='user'" />
       </div>
       <footer class="admin-footer">
-        <p class="admin-copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/eooce/Nav-Item" target="_blank" class="footer-link">Powered by eooce</a></p>
+        <p class="admin-copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/lycc17/nav-item-cf" target="_blank" class="footer-link">Powered by lycc17</a></p>
       </footer>
     </main>
   </div>
