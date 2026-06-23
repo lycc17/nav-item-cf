88c8401bc76a3c9ecceff28d325050a608685a23
lycc17 <155903423+lycc17@users.noreply.github.com>
2026-06-23 16:36:19 +0800

    Update Home.vue

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -63,7 +63,7 @@
           </svg>
           友情链接
         </button>
-        <p class="copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/eooce/Nav-Item" target="_blank" class="footer-link">Powered by eooce</a></p>
+        <p class="copyright">Copyright © 2025 Nav-Item | <a href="https://github.com/lycc17/nav-item-cf" target="_blank" class="footer-link">Powered by lycc17</a></p>
       </div>
     </footer>
 
